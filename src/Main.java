2c9705affaa118f67438e23fd6a859dda923c789
owenoconnell1 <131276489+owenoconnell1@users.noreply.github.com>
2025-02-07 10:29:31 -0500

    Commit #2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        final double TAX_RATE = .02;
+        final double TAX_RATE = .05;
         double price = 20.5;
         double tax = price * TAX_RATE;
         double total = price + tax;
